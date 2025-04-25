ac7853fac3bea2ee50d4ab3373cbe3a3b27b8684
Erik Karwatowski <ekarwatowski@gmail.com>
2025-04-25 17:47:14 -0400

    render advocates function and classing for styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,147 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { ChangeEventHandler, useEffect, useState } from "react";
 import { Advocate } from "./types";
+import { debounce } from "lodash";
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState<Advocate[]>([]);
   const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.data);
-        setFilteredAdvocates(jsonResponse.data);
-      });
-    });
-  }, []);
-
-  const onChange = (e) => {
-    const searchTerm = e.target.value;
-
-    document.getElementById("search-term").innerHTML = searchTerm;
-
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate: Advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.toString().includes(searchTerm) ||
-        advocate.phoneNumber.toString().startsWith(searchTerm)
-      );
-    });
-
-    setFilteredAdvocates(filteredAdvocates);
+  const handleGetAdvocates = async () => {
+    const params = new URLSearchParams();
+    params.append("search", searchTerm);
+    try {
+      console.log(searchTerm);
+      const response = await fetch(`/api/advocates?${params.toString()}`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setFilteredAdvocates(data.data);
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  const onClick = () => {
-    console.log(advocates);
-    setFilteredAdvocates(advocates);
+  const handleSearch: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleReset = () => {
+    setSearchTerm("");
+  };
+
+  useEffect(() => {
+    debounce(handleGetAdvocates, 500)();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchTerm]);
+
+  const renderAdvocates = () => {
+    if (!isLoading && filteredAdvocates.length > 0) {
+      return filteredAdvocates.map((advocate: Advocate) => (
+        <tr key={advocate.id} className="px-3 py-2 whitespace-nowrap">
+          <td className="px-3 py-2 whitespace-nowrap">{advocate.firstName}</td>
+          <td className="px-3 py-2 whitespace-nowrap">{advocate.lastName}</td>
+          <td className="px-3 py-2 whitespace-nowrap">{advocate.city}</td>
+          <td className="px-3 py-2 whitespace-nowrap">{advocate.degree}</td>
+          <td className="px-3 py-2 whitespace-nowrap space-y-2 max-w-lg text-balance">
+            {advocate.specialties.map((s: string, index: number) => (
+              <span key={index} className="specialty-chip inline-block mx-1">
+                {s}
+              </span>
+            ))}
+          </td>
+          <td className="px-3 py-2 whitespace-nowrap">
+            {advocate.yearsOfExperience}
+          </td>
+          <td className="px-3 py-2 whitespace-nowrap">
+            {advocate.phoneNumber}
+          </td>
+        </tr>
+      ));
+    } else if (isLoading) {
+      return (
+        <tr>
+          <td colSpan={7} className="text-center py-4">
+            Loading...
+          </td>
+        </tr>
+      );
+    }
+    return (
+      <tr>
+        <td colSpan={7} className="text-center py-4">
+          No advocates found
+          {searchTerm.length > 0 && (
+            <p className="text-xs text-gray-700 mt-2">
+              Searching for: <span id="search-term">{searchTerm}</span>{" "}
+            </p>
+          )}
+        </td>
+      </tr>
+    );
   };
 
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
+    <main className="min-h-screen p-24 max-w-7xl mx-auto space-y-8">
       <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+        <h1 className="advocate-name">Solace Advocates</h1>
       </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate: Advocate) => {
-            return (
-              <tr key={advocate.id}>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s: string, index: number) => (
-                    <div key={index}>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="flex gap-4 justify-between items-center">
+        <label htmlFor="Search">
+          <p className="text-base text-gray-700">
+            Search
+            {searchTerm.length > 0 && (
+              <span className="ml-4 text-xs text-gray-700 mt-2">
+                Searching for: <span id="search-term">{searchTerm}</span>{" "}
+              </span>
+            )}
+          </p>
+
+          <input
+            type="text"
+            id="Search"
+            onChange={handleSearch}
+            className="mt-0.5 w-full rounded border-black border-2 px-5 py-3 max-w-sm shadow-sm sm:text-sm"
+            value={searchTerm}
+          />
+        </label>
+
+        <button
+          className={`reset-search-button flex px-8 py-3 mt-8 align-center justify-center max-w-xs transition-all ease-in ${
+            searchTerm.length > 0 && !isLoading ? "visible" : "hidden"
+          }`}
+          onClick={handleReset}
+        >
+          Reset Search
+        </button>
+      </div>
+
+      <div className="overflow-x-auto shadow-sm rounded-lg">
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white px-3 py-2 shadow-sm">
+          <thead className="text-left">
+            <tr>
+              <th className="px-3 py-2 whitespace-nowrap">First Name</th>
+              <th className="px-3 py-2 whitespace-nowrap">Last Name</th>
+              <th className="px-3 py-2 whitespace-nowrap">City</th>
+              <th className="px-3 py-2 whitespace-nowrap">Degree</th>
+              <th className="px-3 py-2 whitespace-nowrap max-w-md">
+                Specialties
+              </th>
+              <th className="px-3 py-2 whitespace-nowrap">
+                Years of Experience
+              </th>
+              <th className="px-3 py-2 whitespace-nowrap">Phone Number</th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {renderAdvocates()}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
