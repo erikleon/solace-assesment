c4538cd240407372c9455c2f79cfb03f1c97a3e0
Erik Karwatowski <ekarwatowski@gmail.com>
2025-04-25 17:04:49 -0400

    use lato font and bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Lato } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Solace Candidate Assignment",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={lato.className + " bg-[#fffdfa]"}>{children}</body>
     </html>
   );
 }
